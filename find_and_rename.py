8b69c6f253bf633f7f4c1b2edd4f898376b97312
huangyq89 <94620372+huangyq89@users.noreply.github.com>
2022-04-13 18:40:54 +0800

    Update find_and_rename.py

--- find_and_rename.py
+++ find_and_rename.py
@@ -1,12 +1,18 @@
 import re
 import os
+import time
 
 def find_and_rename(annotation_file, sequence_file):
-    sentence = 'awk \'/^>/{if (l!=\"\") print l; print; l=0; next}{l+=length($0)}END{print l}\' ' + sequence_file + ' > sequence_read.txt'
-    os.system(sentence)
-    f = open('sequence_read.txt', 'r')
+    f = open(sequence_file, 'r')
     a = f.readlines()
     f.close()
+    num = 0
+    for line in a:
+        if re.match("^>", line) is not None:
+            num += 1
+    print('Received ' + str(num) + ' sequences as input.')
+    print(((time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))) + \
+    ' Start looking for and renaming duplicated genes in the old annotation file.')
     f = open(annotation_file, 'r')
     b = f.readlines()
     f.close()
@@ -15,12 +21,13 @@ def find_and_rename(annotation_file, sequence_file):
         if re.match("^>", line) is not None:
             chromo = re.findall(r'>(.*?)\s', line)[0]
             for line in b:
-                if chromo.lower() in line:
+                if '"' + chromo + '"' or '-' + chromo + ';' in line:
                     f.writelines(line)
-                    b[b.index(line)] = line.replace(chromo.lower(), chromo.lower() + "_old")
+                    b[b.index(line)] = line.replace(chromo, chromo + "_old")
                     break
     f.close()
     f = open('rename.gtf', 'w')
     f.writelines(b)
     f.close()
-
+    print(((time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))) + \
+    ' Done!')
