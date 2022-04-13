a4ecfdba07c11c34fe8b1007e006ab8629199ec4
huangyq89 <94620372+huangyq89@users.noreply.github.com>
2022-04-13 19:41:06 +0800

    Update add_genome.py

--- add_genome.py
+++ add_genome.py
@@ -1,5 +1,9 @@
 import os
+import time
+
 def add_genome(sequence_file, output_genome):
+    print(((time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))) + \
+    ' Start adding sequences of new genes to the reference genome.')
     f = open(sequence_file, 'r')
     a = f.readlines()
     f.close()
@@ -8,3 +12,5 @@ def add_genome(sequence_file, output_genome):
     f.writelines(a)
     f.close()
     os.system('mv mask_result_2.txt ' + output_genome)
+    print(((time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))) + \
+    ' Done!')
